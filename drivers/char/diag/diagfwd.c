36dc1d92a4b1bbf96741a93c85207c42158d8bcd
Stepan Moskovchenko <stepanm@codeaurora.org>
2012-09-21 20:32:17 -0700

    msm: Refactor cpu_is macros into SoC classes
    Refactor common combinations of cpu_is macros into logical
    SoC class macros to reduce the maintenance burden of
    implementing support for additional product variants.
    
    Change-Id: I6472eb70fa43e1217ff3ebb3643288196b9864b5
    Signed-off-by: Stepan Moskovchenko <stepanm@codeaurora.org>
    
    Conflicts:
    	arch/arm/mach-msm/include/mach/socinfo.h
    	arch/arm/mach-msm/platsmp.c
    	arch/arm/mach-msm/rpm_resources.c
    	arch/arm/mach-msm/spm_devices.c
    	arch/arm/mach-msm/timer.c
    	drivers/char/diag/diagfwd.c
    	sound/soc/msm/apq8064-i2s.c
    	sound/soc/msm/apq8064.c
    	sound/soc/msm/msm8930.c
    	sound/soc/msm/msm8960.c

--- drivers/char/diag/diagfwd.c
+++ drivers/char/diag/diagfwd.c
@@ -162,14 +162,8 @@ int chk_apps_master(void)
 {
 	if (driver->use_device_tree)
 		return 1;
-	#if 0
 	else if (soc_class_is_msm8960() || soc_class_is_msm8930() ||
 		 soc_class_is_apq8064() || cpu_is_msm9615())
-	#else
-	else if (cpu_is_msm8960() || cpu_is_msm8930() || cpu_is_msm8930aa() ||
-			cpu_is_msm9615() || cpu_is_apq8064() || cpu_is_msm8627() ||
-			cpu_is_msm8960ab() || cpu_is_apq8064ab())
-	#endif
 		return 1;
 	else
 		return 0;
